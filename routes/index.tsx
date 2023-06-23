88e2a1ba0f7e1f4ba8b6f9a5f435184b2fd289ce
David Balan <sc@silvercrow.org>
2023-06-23 00:37:56 -0400

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -47,7 +47,9 @@ export default function Home() {
       <div class="footer">
         <a href="https://github.com/silvercrow" target="_blank">© David Balan 
       </a>
-        <a href="https://bmc.link/tquickbrownfox" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+        <a href="https://bmc.link/tquickbrownfox" target="_blank">
+          <img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" />
+        </a>
       </div>
     </>
   );
